5dc82293500f1cf525dd741b3bac75e1d5d725b9
shashankkatte <shashankanand.katte@gmail.com>
2021-02-27 12:18:31 -0500

    src : Initial App scaffolding
    App clean up with CRA redux template
    Firebase config
    React router install and config

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="app">
+      <Router>
+        <>
+          <Switch>
+            <Route path="/">
+              <h1>This home page</h1>
+            </Route>
+          </Switch>
+        </>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDIW-mlVMym0funfNns6J5koDuLsWuH5HE",
+    authDomain: "slack-react-f2766.firebaseapp.com",
+    projectId: "slack-react-f2766",
+    storageBucket: "slack-react-f2766.appspot.com",
+    messagingSenderId: "1039148946113",
+    appId: "1:1039148946113:web:2a1d3ed38a2364bb88ba40",
+    measurementId: "G-5G9EHEC2EM"
+  };
