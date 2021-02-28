dec7d075f02364fe961d30fa2ab25d186b3fe1a0
shashankkatte <shashankanand.katte@gmail.com>
2021-02-28 00:02:04 -0500

    src : Chat section header added

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,10 +1,26 @@
+import { StarBorderOutlined } from '@material-ui/icons';
 import React from 'react';
 import styled from 'styled-components';
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
 
 function Chat() {
   return (
     <ChatContainer>
-      <h1>Chat Screen!</h1>
+      <Header>
+        <HeaderLeft>
+          <h4>
+            <strong>#Room-name</strong>
+          </h4>
+          <StarBorderOutlined />
+        </HeaderLeft>
+
+        <HeaderRight>
+          <p>
+            <InfoOutlinedIcon />
+            Details
+          </p>
+        </HeaderRight>
+      </Header>
     </ChatContainer>
   );
 }
@@ -15,5 +31,40 @@ const ChatContainer = styled.div`
   flex: 0.7;
   flex-grow: 1;
   overflow-y: scroll;
-  margin-top:60px;
+  margin-top: 60px;
+`;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
+  border-bottom: 1px solid lightgray;
+`;
+
+const HeaderLeft = styled.div`
+  display: flex;
+  align-items: center;
+  > h4 {
+    display: flex;
+    text-transform: lowercase;
+    margin-right: 10px;
+  }
+
+  > h4 > .MuiSvgIcon-root {
+    margin-left: 10px;
+    font-size: 18px;
+  }
+`;
+
+const HeaderRight = styled.div`
+  > p {
+    display: flex;
+    align-items: center;
+    font-size: 14px;
+  }
+
+  > p > .MuvSvgIcon-root {
+    margin-right: 5px !important;
+    font-size: 16px;
+  }
 `;
