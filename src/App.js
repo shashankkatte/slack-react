8072cd1befc9acc00ec3eb8f5e1875e8a5262cd9
shashankkatte <shashankanand.katte@gmail.com>
2021-02-27 23:45:44 -0500

    src : Add Chat component basic boilerplate

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import styled from 'styled-components';
+import Chat from './components/Chat';
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
             <Sidebar />
             <Switch>
               <Route path="/" exact>
-                {/* Chat */}
+                <Chat />
               </Route>
             </Switch>
           </AppBody>
