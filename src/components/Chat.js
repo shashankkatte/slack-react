a79e05a94d647f6f06b526d8d5419db36145a490
shashankkatte <shashankanand.katte@gmail.com>
2021-02-28 04:11:25 -0500

    src : First load user message
    Add a message for user to select a channel

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -32,7 +32,7 @@ function Chat() {
 
   return (
     <ChatContainer>
-      {roomDetails && roomMessages && (
+      { (roomDetails && roomMessages ? (
         <>
           <Header>
             <HeaderLeft>
@@ -70,6 +70,19 @@ function Chat() {
             channelName={roomDetails?.data().name}
             channelId={roomId}
           />
+        </>
+      ) : (
+        <>
+          <LoginContainer>
+            <LoginInnerContainer>
+              <img
+                src="https://cdn.mos.cms.futurecdn.net/SDDw7CnuoUGax6x9mTo7dd.jpg"
+                alt=""
+              />
+              <h1>Select a Channel on the left to start</h1>
+              <p>katteio.slack.com</p>
+            </LoginInnerContainer>
+          </LoginContainer>
         </>
       )}
     </ChatContainer>
@@ -121,7 +134,30 @@ const HeaderRight = styled.div`
 `;
 
 const ChatMessages = styled.div`
-  margin-bottom:150px;
+  margin-bottom: 150px;
 `;
 
 const ChatBottom = styled.div``;
+
+const LoginContainer = styled.div`
+  background-color: #f8f8f8;
+  height: 100vh;
+  display: grid;
+  place-items: center;
+`;
+const LoginInnerContainer = styled.div`
+  padding: 100px;
+  text-align: center;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+
+  > img {
+    object-fit: contain;
+    height: 100px;
+    margin-bottom: 40px;
+  }
+
+ 
+`;
+
